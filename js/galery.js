fce0110604f75622175a380baa507aca591c1a02
Felipe Chiquito <felipechiquito010@gmail.com>
2022-03-04 10:34:10 -0500

    Repository Update

--- js/galery.js
+++ js/galery.js
@@ -7,7 +7,7 @@ function Siguiente(){
         num = 1;
     }
     var imagen = document.getElementById("image");
-    imagen.src="../img/PA/PA" + num + ".jpg";   
+    imagen.src="../img/catalogoArte/arte/PA" + num + ".jpg";   
 };
 
 function Anterior(){
@@ -16,6 +16,222 @@ function Anterior(){
         num = 22;
     }
     var image = document.getElementById("image");
-    image.src="../img/PA/PA" + num + ".jpg";
+    image.src="../img/catalogoArte/arte/PA" + num + ".jpg";
     
+}
+function SiguienteE(){
+    num++; 
+    if (num > 22) {
+        num = 1;
+    }
+    var imagen = document.getElementById("imageE");
+    imagen.src="../img/catalogoArte/escolares/PA" + num + ".jpg";   
+};
+
+function AnteriorE(){
+    num--;
+    if (num < 1) {
+        num = 22;
+    }
+    var image = document.getElementById("imageE");
+    image.src="../img/catalogoArte/escolares/PA" + num + ".jpg";
+    
+}
+function SiguienteP(){
+    num++; 
+    if (num > 22) {
+        num = 1;
+    }
+    var imagen = document.getElementById("imageP");
+    imagen.src="../img/catalogoArte/pinceles/PA" + num + ".jpg";   
+};
+
+function AnteriorP(){
+    num--;
+    if (num < 1) {
+        num = 22;
+    }
+    var image = document.getElementById("imageP");
+    image.src="../img/catalogoArte/pinceles/PA" + num + ".jpg";
+    
+}
+/* JS portafoio de Bisturí */
+function SiguienteBs(){-
+    num++; 
+    if (num > 6) {
+        num = 1;
+    }
+    var imagen = document.getElementById("imageBs");
+    imagen.src="../img/catalogoOficina/bistury/PA" + num + ".jpg";   
+};
+
+function AnteriorBs(){
+    num--;
+    if (num < 1) {
+        num = 6;
+    }
+    var image = document.getElementById("imageBs");
+    image.src="../img/catalogoOficina/bistury/PA" + num + ".jpg";
+}
+/* Js portafolio de Cacharreria */
+function SiguienteCh(){
+    num++; 
+    if (num > 19) {
+        num = 1;
+    }
+    var imagen = document.getElementById("imageCh");
+    imagen.src="../img/catalogoOficina/cacharreria/PA" + num + ".jpg";   
+};
+
+function AnteriorCh(){
+    num--;
+    if (num < 1) {
+        num = 19;
+    }
+    var image = document.getElementById("imageCh");
+    image.src="../img/catalogoOficina/cacharreria/PA" + num + ".jpg";
+}
+/* Js portafolio de Calculadora */
+function SiguienteCl(){
+    num++; 
+    if (num > 6) {
+        num = 1;
+    }
+    var imagen = document.getElementById("imageCl");
+    imagen.src="../img/catalogoOficina/calculadora/PA" + num + ".jpg";   
+};
+
+function AnteriorCl(){
+    num--;
+    if (num < 1) {
+        num = 6;
+    }
+    var image = document.getElementById("imageCl");
+    image.src="../img/catalogoOficina/calculadora/PA" + num + ".jpg";
+}
+/* Js portafolio de Cintas */
+function SiguienteCt(){
+    num++; 
+    if (num > 8) {
+        num = 1;
+    }
+    var imagen = document.getElementById("imageCt");
+    imagen.src="../img/catalogoOficina/cintas/PA" + num + ".jpg";   
+};
+
+function AnteriorCt(){
+    num--;
+    if (num < 1) {
+        num = 8;
+    }
+    var image = document.getElementById("imageCt");
+    image.src="../img/catalogoOficina/cintas/PA" + num + ".jpg";
+}
+/* Js portafolio de Cosedoras */
+function SiguienteCds(){
+    num++; 
+    if (num > 11) {
+        num = 1;
+    }
+    var imagen = document.getElementById("imageCds");
+    imagen.src="../img/catalogoOficina/cosedoras/PA" + num + ".jpg";   
+};
+
+function AnteriorCds(){
+    num--;
+    if (num < 1) {
+        num = 11;
+    }
+    var image = document.getElementById("imageCds");
+    image.src="../img/catalogoOficina/cosedoras/PA" + num + ".jpg";
+}
+/* Js portafolio de Esferos */
+function SiguienteEs(){
+    num++; 
+    if (num > 8) {
+        num = 1;
+    }
+    var imagen = document.getElementById("imageEs");
+    imagen.src="../img/catalogoOficina/esferos/PA" + num + ".jpg";   
+};
+
+function AnteriorEs(){
+    num--;
+    if (num < 1) {
+        num = 8;
+    }
+    var image = document.getElementById("imageEs");
+    image.src="../img/catalogoOficina/esferos/PA" + num + ".jpg";
+}
+/* Js portafolio de Ganchos */
+function SiguienteGc(){
+    num++; 
+    if (num > 10) {
+        num = 1;
+    }
+    var imagen = document.getElementById("imageGc");
+    imagen.src="../img/catalogoOficina/ganchos/PA" + num + ".jpg";   
+};
+
+function AnteriorGc(){
+    num--;
+    if (num < 1) {
+        num = 10;
+    }
+    var image = document.getElementById("imageGc");
+    image.src="../img/catalogoOficina/ganchos/PA" + num + ".jpg";
+}
+/* Js portafolio de Notas Adhesivas */
+function SiguienteNa(){
+    num++; 
+    if (num > 5) {
+        num = 1;
+    }
+    var imagen = document.getElementById("imageNa");
+    imagen.src="../img/catalogoOficina/notasAdhesivas/PA" + num + ".jpg";   
+};
+
+function AnteriorNa(){
+    num--;
+    if (num < 1) {
+        num = 5;
+    }
+    var image = document.getElementById("imageGc");
+    image.src="../img/catalogoOficina/notasAdhesivas/PA" + num + ".jpg";
+}
+/* Js portafolio para la Oficina */
+function SiguienteOf(){
+    num++; 
+    if (num > 28) {
+        num = 1;
+    }
+    var imagen = document.getElementById("imageOf");
+    imagen.src="../img/catalogoOficina/oficina/PA" + num + ".jpg";   
+};
+
+function AnteriorOf(){
+    num--;
+    if (num < 1) {
+        num = 28;
+    }
+    var image = document.getElementById("imageOf");
+    image.src="../img/catalogoOficina/oficina/PA" + num + ".jpg";
+}
+/* Js portafolio de perforadoras */
+function SiguientePr(){
+    num++; 
+    if (num > 11) {
+        num = 1;
+    }
+    var imagen = document.getElementById("imagePr");
+    imagen.src="../img/catalogoOficina/perforadora/PA" + num + ".jpg";   
+};
+
+function AnteriorPr(){
+    num--;
+    if (num < 1) {
+        num = 11;
+    }
+    var image = document.getElementById("imagePr");
+    image.src="../img/catalogoOficina/perforadora/PA" + num + ".jpg";
 }
